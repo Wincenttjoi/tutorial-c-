a5449fef558ecdf30682ded335d09e600b6fe3f2
wincenttjoi <wincenttsjoi@gmail.com>
2021-07-20 15:33:05 +0800

    Inheritance concept

--- hello.cpp
+++ hello.cpp
@@ -59,12 +59,25 @@ public:
   }
 };
 
+class Developer:public Employee {
+public:
+  string FavProgrammingLanguage;
+  Developer(string name, string company, int age, string favProgrammingLanguage) :Employee(name, company, age) {
+    FavProgrammingLanguage = favProgrammingLanguage;
+  }
+
+  void FixBug() {
+    std::cout << getName() << " fix bug using " << FavProgrammingLanguage << std::endl;
+  }
+
+};
+
 int main()
 {
   Employee employee1 = Employee("Wincent", "Shopee", 25);
   employee1.IntroduceYourself();
   employee1.AskForPromotion();
-  
+
   Employee employee2 = Employee("John", "Shopee", 35);
   employee2.IntroduceYourself();
   employee2.AskForPromotion();
@@ -72,4 +85,8 @@ int main()
 
   employee1.setAge(39);
   std::cout << employee1.getName() << " is " << employee1.getAge() << " years old" << std::endl;
+
+  Developer d = Developer("Ace", "Google", 5, "C++");
+  d.FixBug();
+  d.AskForPromotion();
 }
